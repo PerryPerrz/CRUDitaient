ab9d727ad77846a14367c4059476b67fe4a0b267
PerryPerrz <iophugstud6@gmail.com>
2023-09-24 10:24:50 +0200

    Enable Cross-Origin Resource Sharing

--- back-end/src/main/java/com/example/Cruditaient/CruditaientApplication.java
+++ back-end/src/main/java/com/example/Cruditaient/CruditaientApplication.java
@@ -2,6 +2,10 @@ package com.example.Cruditaient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CruditaientApplication {
@@ -10,4 +14,16 @@ public class CruditaientApplication {
 		SpringApplication.run(CruditaientApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(java.util.Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(java.util.Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(java.util.Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
